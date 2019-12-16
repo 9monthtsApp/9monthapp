d242102d6f4218fbe8dbae6fe2e235ef829b13a9
Maycohen60 <57867185+Maycohen60@users.noreply.github.com>
2019-12-16 11:42:55 +0200

    adding pregnancy calendar adding things to avoid articles

--- app/src/main/java/com/first/a9monthsproject/homePage.java
+++ app/src/main/java/com/first/a9monthsproject/homePage.java
@@ -2,20 +2,10 @@ package com.first.a9monthsproject;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class homePage extends AppCompatActivity {
 
@@ -27,43 +17,10 @@ public class homePage extends AppCompatActivity {
     private Button articlesButton;
     private Button logOutButton;
 
-    private FirebaseDatabase mDatabase;
-    private DatabaseReference mDatabaseReference;
-    private FirebaseAuth firebaseAuth;
-    private DatabaseReference mDb;
-    private static final String TAG = "MainActivity";
-    public String userId;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-
-        firebaseAuth = FirebaseAuth.getInstance();
-        mDatabase = FirebaseDatabase.getInstance();
-        mDb = mDatabase.getReference();
-        FirebaseUser user = firebaseAuth.getCurrentUser();
-        String userKey = user.getUid();
-        Toast.makeText(homePage.this, "week" + userKey , Toast.LENGTH_LONG).show();
-
-
-        mDb.child("MUsers").child(userKey).child("First_name").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                userId = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Name: " + userId);
-
-                System.out.println (userId);
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {}
-        });
-
-
-
 
         personalAreaButton = (Button) findViewById(R.id.personal_area);
         myTestsButton = (Button) findViewById(R.id.testResults);
