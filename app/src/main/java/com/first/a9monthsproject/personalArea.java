16ff63a5f2ab31700c2e2d0705e6225eb5968d0c
Maycohen60 <57867185+Maycohen60@users.noreply.github.com>
2019-12-22 17:16:26 +0200

    changing pics

--- app/src/main/java/com/first/a9monthsproject/personalArea.java
+++ app/src/main/java/com/first/a9monthsproject/personalArea.java
@@ -1,12 +1,11 @@
 package com.first.a9monthsproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class personalArea extends AppCompatActivity {
 
@@ -40,6 +39,8 @@ public class personalArea extends AppCompatActivity {
                 openBabySizePage();
             }
         });
+
+
     }
 
     private void openBabySizePage() {
@@ -51,6 +52,7 @@ public class personalArea extends AppCompatActivity {
         Intent in = new Intent(this, hoosingOptionImageAlbum.class);
         startActivity(in);
     }
+
 
 
 }
