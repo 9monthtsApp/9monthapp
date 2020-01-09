340523f71818cce5cbe30aa6c37f3ad9834095e7
timor <talfassy23@gmail.com>
2020-01-09 15:05:03 +0200

    decoding blood tests + info about each tests result

--- app/src/main/java/com/first/a9monthsproject/decodingResults.java
+++ app/src/main/java/com/first/a9monthsproject/decodingResults.java
@@ -32,14 +32,27 @@ import java.util.List;
 public class decodingResults extends AppCompatActivity {
 
     private TextView title;
-    private TextView checkWBC;
-    private TextView checkRBC;
     private ListView list;
     private Button returnBtn;
     List<String> keyList;
     ArrayList<String> arrayList;
     ArrayAdapter<String> arrayAdapter;
 
+//check test value
+    private TextView checkWBC;
+    private TextView checkRBC;
+    private TextView checkGlucose;
+    public TextView checkHemoglobin;
+    private TextView checkHematocrit;
+    private TextView checkMCV;
+    private TextView checkMCH;
+    private TextView checkMCHC;
+    private TextView checkRDW;
+    private TextView checkPlatelets;
+    public TextView checkMPV;
+
+
+    //firebase
     private FirebaseDatabase mDatabase;
     private DatabaseReference mDatabaseReference;
     private FirebaseAuth mAuth;
@@ -56,8 +69,20 @@ public class decodingResults extends AppCompatActivity {
         returnBtn =(Button) findViewById(R.id.returnButton);
 
         //decoding per tests
+       checkGlucose =(TextView) findViewById(R.id.Glucose);
         checkWBC =(TextView) findViewById(R.id.WBC);
-        checkRBC =(TextView) findViewById(R.id.RBC) ;
+        checkRBC =(TextView) findViewById(R.id.RBC);
+        checkHemoglobin =(TextView) findViewById(R.id.Hemoglobin);
+
+        checkHematocrit =(TextView) findViewById(R.id.Hematocrit);
+        checkMCV =(TextView) findViewById(R.id.MCV);
+        checkMCH = (TextView) findViewById(R.id.MCH);
+        checkMCHC = (TextView) findViewById(R.id.MCHC);
+        checkRDW = (TextView) findViewById(R.id.RDW);
+        checkPlatelets = (TextView) findViewById(R.id.Platelets);
+        checkMPV = (TextView) findViewById(R.id.MPV);
+
+
 
         arrayList = new ArrayList<String>();
         keyList = new ArrayList<String>();
@@ -121,9 +146,18 @@ public class decodingResults extends AppCompatActivity {
                 String resultValue = arrayAdapter.getItem(position);
                 String[] result = resultValue.split(" ");
 
-               checkValuesWBC(result);
-               checkValuesRBC(result);
+                 checkValuesGlucose(result);
+                checkValuesWBC(result);
+                checkValuesRBC(result);
+                checkValuesHemoglobin(result);
 
+                checkValuesHematocrit(result);
+                checkValuesMCV(result);
+                checkValuesMCH(result);
+                checkValuesMCHC(result);
+                checkValuesRDW(result);
+                checkValuesPlatelets(result);
+                checkValuesMPV(result);
 
 
 
@@ -136,28 +170,142 @@ public class decodingResults extends AppCompatActivity {
 
 
     }
-    private void checkValuesWBC( String[] str){
-        if (Double.valueOf(str[3]) < 14.00) {
-            checkWBC.setText("WBC-this test examines a number of white blood cells - your WBC test values ​​are under the normal range!");
 
-        } else if (Double.valueOf(str[3]) > 16.00) {
-            checkWBC.setText("WBC-this test examines a number of white blood cells - your WBC test values are above the normal range ! ");
+    private void checkValuesGlucose( String[] str){
+
+        if (Double.valueOf(str[13]) < 65.00) {
+            checkGlucose.setText("Glucose-this test examines a number of red blood cells -  the amount of sugar in your blood below the proper range!");
+
+        } else if (Double.valueOf(str[13]) > 126.0) {
+            checkGlucose.setText("Glucose-this test examines the amount of sugar in the blood - the amount of sugar in your blood above the proper range! ");
 
         } else{
-            checkWBC.setText("WBC-this test examines a number of white blood cells - your WBC test values are at the normal range "); }
+            checkGlucose.setText("Glucose-this test examines a number of red blood cells -  the amount of sugar in your blood are at the proper range! "); }
+    }
+
+    private void checkValuesWBC( String[] str){
+        if (Double.valueOf(str[14]) < 4.5) {
+            checkWBC.setText("WBC-this test examines a number of white blood cells - your WBC values ​​are below the normal range!");
+
+        } else if (Double.valueOf(str[14]) > 11.00) {
+            checkWBC.setText("WBC-this test examines a number of white blood cells - your WBC values are above the normal range ! ");
+
+        } else{
+            checkWBC.setText("WBC-this test examines a number of white blood cells - your WBC values are at the normal range. "); }
     }
 
     private void checkValuesRBC( String[] str){
 
-        if (Double.valueOf(str[5]) < 3.00) {
-            checkRBC.setText("RBC-this test examines a number of red blood cells - your RBC test values ​​are under the normal range!");
+        if (Double.valueOf(str[15]) < 3.5) {
+            checkRBC.setText("RBC-this test examines a number of red blood cells - your RBC values ​​are below the normal range!");
 
-        } else if (Double.valueOf(str[5]) > 5.5) {
-            checkRBC.setText("RBC-this test examines a number of red blood cells - your RBC test values are above the normal range ! ");
+        } else if (Double.valueOf(str[15]) > 5.5) {
+            checkRBC.setText("RBC-this test examines a number of red blood cells - your RBC values are above the normal range ! ");
 
         } else{
-            checkRBC.setText("RBC-this test examines a number of red blood cells - your RBC test values are at the normal range "); }
+            checkRBC.setText("RBC-this test examines a number of red blood cells - your RBC values are at the normal range."); }
     }
+
+    private void checkValuesHemoglobin( String[] str){
+
+        if (Double.valueOf(str[16]) < 11.0) {
+            checkHemoglobin.setText("Hemoglobin- this test examines the blood hemoglobin level - your hemoglobin values ​​are below the normal range!");
+
+        } else if (Double.valueOf(str[16]) > 16.0) {
+            checkHemoglobin.setText("Hemoglobin- this test examines the blood hemoglobin level - your hemoglobin values ​​are above the normal range! ");
+
+        } else{
+            checkHemoglobin.setText("Hemoglobin- this test examines the blood hemoglobin level - your hemoglobin values ​​are at the normal range"); }
+    }
+
+    private void checkValuesHematocrit( String[] str){
+
+        if (Double.valueOf(str[17]) < 36.0) {
+            checkHematocrit.setText("Hematocrit- this test counting the percentage of red blood cell volume - The percentage of red blood cell volume is low !");
+
+        } else if (Double.valueOf(str[17]) > 46.0) {
+            checkHematocrit.setText("Hematocrit- this test counting the percentage of red blood cell volume - The percentage of red blood cell volume is high !");
+
+        } else{
+            checkHematocrit.setText("Hematocrit- this test counting the percentage of red blood cell volume - The percentage of red blood cell volume is at the normal range."); }
+    }
+
+    private void checkValuesMCV( String[] str){
+
+        if (Double.valueOf(str[18]) < 79.0) {
+            checkMCV.setText("MCV- The test counts the volume of red blood cells - your volume of red blood cells ​​are below the normal range!");
+
+        } else if (Double.valueOf(str[18]) > 97.0) {
+            checkMCV.setText("MCV- The test counts the volume of red blood cells- your volume of red blood cells are above the normal range! ");
+
+        } else{
+            checkMCV.setText("MCV- The test counts the volume of red blood cells - your volume of red blood cells ​​are at the normal range"); }
+    }
+
+    private void checkValuesMCH( String[] str){
+
+        if (Double.valueOf(str[19]) < 27.0) {
+            checkMCH.setText("MCH- The test checking the amount and concentration of hemoglobin in the blood cells - the concentration of hemoglobin in your blood cells are low!");
+
+        } else if (Double.valueOf(str[19]) > 34.0) {
+            checkMCH.setText("MCH- The test checking the amount and concentration of hemoglobin in the blood cells - the concentration of hemoglobin in your blood cells are high! ");
+
+        } else{
+            checkMCH.setText("MCH- The test checking the amount and concentration of hemoglobin in the blood cells - the concentration of hemoglobin in your blood cells are at the normal range."); }
+    }
+
+    private void checkValuesMCHC( String[] str){
+
+        if (Double.valueOf(str[20]) < 31.0) {
+            checkMCHC.setText("MCHC- The test reflects the amount of hemoglobin relative to the cell size for each red blood cell - The amount of hemoglobin relative to the cell size is lower than the normal range!");
+
+        } else if (Double.valueOf(str[20]) > 36.0) {
+            checkMCHC.setText("MCHC- The test reflects the amount of hemoglobin relative to the cell size for each red blood cell - The amount of hemoglobin relative to the cell size is higher than the normal range! ");
+
+        } else{
+            checkMCHC.setText("MCHC- The test reflects the amount of hemoglobin relative to the cell size for each red blood cell - The amount of hemoglobin relative to the cell size is at the normal range"); }
+    }
+
+    private void checkValuesRDW( String[] str){
+
+        if (Double.valueOf(str[21]) < 11.5) {
+            checkRDW.setText("RDW- Testing the variability of red blood cell volumes - the variability of red blood cell volumes is below the normal range!");
+
+        } else if (Double.valueOf(str[21]) > 15.0) {
+            checkRDW.setText("RDW- Testing the variability of red blood cell volumes - the variability of red blood cell volumes is above the normal range! ");
+
+        } else{
+            checkRDW.setText("RDW- Testing the variability of red blood cell volumes - the variability of red blood cell volumes is at the normal range"); }
+    }
+
+    private void checkValuesPlatelets( String[] str){
+
+        if (Double.valueOf(str[22]) < 150.0) {
+            checkPlatelets.setText("Platelets- Count the amount of platelets in the blood - The amount of platelets in your blood is low!");
+
+        } else if (Double.valueOf(str[22]) > 450.0) {
+            checkPlatelets.setText("Platelets- Count the amount of platelets in the blood - The amount of platelets in your blood is high!");
+
+        } else{
+            checkPlatelets.setText("Platelets- Count the amount of platelets in the blood - The amount of platelets in your blood are at the normal range"); }
+    }
+
+    private void checkValuesMPV( String[] str) {
+
+        if (Double.valueOf(str[23]) < 8.5) {
+            checkMPV.setText("MPV- The test measures an average platelet volume - The average blood platelet volume in your blood is low!");
+
+        } else if (Double.valueOf(str[23]) > 12.9) {
+            checkMPV.setText("MPV- The test measures an average platelet volume - The average blood platelet volume in your blood is high!");
+
+        } else {
+            checkMPV.setText("MPV- The test measures an average platelet volume - The average blood platelet volume in your blood is at the normal range");
+        }
+
+    }
+
+
+
 
     private void openTestsPage() {
         Intent in = new Intent(this , tests.class);
--- app/src/main/java/com/first/a9monthsproject/watch_tests_results.java
+++ app/src/main/java/com/first/a9monthsproject/watch_tests_results.java
@@ -6,8 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -27,8 +30,25 @@ public class watch_tests_results extends AppCompatActivity {
 
     private ListView list;
     private Button returnBtn;
+    private Button explanationsButton;
+    private ImageButton logoButton;
     ArrayList<String> arrayList;
     ArrayAdapter<String> arrayAdapter;
+
+    //check test value
+    private TextView checkWBC;
+    private TextView checkRBC;
+    private TextView checkGlucose;
+    public TextView checkHemoglobin;
+    private TextView checkHematocrit;
+    private TextView checkMCV;
+    private TextView checkMCH;
+    private TextView checkMCHC;
+    private TextView checkRDW;
+    private TextView checkPlatelets;
+    public TextView checkMPV;
+
+
 
     private FirebaseDatabase mDatabase;
     private DatabaseReference mDatabaseReference;
@@ -45,6 +65,23 @@ public class watch_tests_results extends AppCompatActivity {
         title = (TextView) findViewById(R.id.bloodTestsResult);
         list = (ListView) findViewById(R.id.listViewId);
         returnBtn =(Button) findViewById(R.id.returnButton);
+        explanationsButton =(Button) findViewById(R.id.seeTestInfo) ;
+        logoButton =(ImageButton) findViewById(R.id.Image_Logo);
+
+        //decoding per tests
+        checkGlucose =(TextView) findViewById(R.id.Glucose);
+        checkWBC =(TextView) findViewById(R.id.WBC);
+        checkRBC =(TextView) findViewById(R.id.RBC);
+        checkHemoglobin =(TextView) findViewById(R.id.Hemoglobin);
+
+        checkHematocrit =(TextView) findViewById(R.id.Hematocrit);
+        checkMCV =(TextView) findViewById(R.id.MCV);
+        checkMCH = (TextView) findViewById(R.id.MCH);
+        checkMCHC = (TextView) findViewById(R.id.MCHC);
+        checkRDW = (TextView) findViewById(R.id.RDW);
+        checkPlatelets = (TextView) findViewById(R.id.Platelets);
+        checkMPV = (TextView) findViewById(R.id.MPV);
+
 
 
         arrayList = new ArrayList<String>();
@@ -67,7 +104,6 @@ public class watch_tests_results extends AppCompatActivity {
                 String value = dataSnapshot.getValue(String.class);
                 arrayList.add(value);
                 arrayAdapter.notifyDataSetChanged();
-
 
             }
 
@@ -92,7 +128,194 @@ public class watch_tests_results extends AppCompatActivity {
             }
         });
 
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String resultValue = arrayAdapter.getItem(position);
+                String[] result = resultValue.split(" ");
+
+                checkValuesGlucose(result);
+                checkValuesWBC(result);
+                checkValuesRBC(result);
+                checkValuesHemoglobin(result);
+
+                checkValuesHematocrit(result);
+                checkValuesMCV(result);
+                checkValuesMCH(result);
+                checkValuesMCHC(result);
+                checkValuesRDW(result);
+                checkValuesPlatelets(result);
+                checkValuesMPV(result);
+
+
+
+
+
+            }// end of onItemClick
+        }); //end of setOnItemClickListener
+
+
+        explanationsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGeneralInfo_blood();
+            }
+        });
+
+        logoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHomePage();
+            }
+        });
+
+
     }
+
+    private void openHomePage() {
+        Intent in = new Intent(this, homePage.class);
+        startActivity(in);
+    }
+
+    private void openGeneralInfo_blood() {
+        Intent in = new Intent(this, Blood_TestGeneralInfo.class);
+        startActivity(in);
+    }
+
+    private void checkValuesGlucose( String[] str){
+
+        if (Double.valueOf(str[13]) < 65.00) {
+            checkGlucose.setText("Glucose-this test examines a number of red blood cells -  the amount of sugar in your blood below the proper range!");
+
+        } else if (Double.valueOf(str[13]) > 130.0) {
+            checkGlucose.setText("Glucose-this test examines the amount of sugar in the blood - the amount of sugar in your blood above the proper range! ");
+
+        } else{
+            checkGlucose.setText("Glucose-this test examines a number of red blood cells -  the amount of sugar in your blood are at the proper range! "); }
+    }
+
+    private void checkValuesWBC( String[] str){
+        if (Double.valueOf(str[14]) < 4.5) {
+            checkWBC.setText("WBC-this test examines a number of white blood cells - your WBC values ​​are below the normal range!");
+
+        } else if (Double.valueOf(str[14]) > 11.00) {
+            checkWBC.setText("WBC-this test examines a number of white blood cells - your WBC values are above the normal range ! ");
+
+        } else{
+            checkWBC.setText("WBC-this test examines a number of white blood cells - your WBC values are at the normal range. "); }
+    }
+
+    private void checkValuesRBC( String[] str){
+
+        if (Double.valueOf(str[15]) < 3.5) {
+            checkRBC.setText("RBC-this test examines a number of red blood cells - your RBC values ​​are below the normal range!");
+
+        } else if (Double.valueOf(str[15]) > 5.5) {
+            checkRBC.setText("RBC-this test examines a number of red blood cells - your RBC values are above the normal range ! ");
+
+        } else{
+            checkRBC.setText("RBC-this test examines a number of red blood cells - your RBC values are at the normal range."); }
+    }
+
+    private void checkValuesHemoglobin( String[] str){
+
+        if (Double.valueOf(str[16]) < 11.0) {
+            checkHemoglobin.setText("Hemoglobin- this test examines the blood hemoglobin level - your hemoglobin values ​​are below the normal range!");
+
+        } else if (Double.valueOf(str[16]) > 16.0) {
+            checkHemoglobin.setText("Hemoglobin- this test examines the blood hemoglobin level - your hemoglobin values ​​are above the normal range! ");
+
+        } else{
+            checkHemoglobin.setText("Hemoglobin- this test examines the blood hemoglobin level - your hemoglobin values ​​are at the normal range"); }
+    }
+
+    private void checkValuesHematocrit( String[] str){
+
+        if (Double.valueOf(str[17]) < 36.0) {
+            checkHematocrit.setText("Hematocrit- this test counting the percentage of red blood cell volume - The percentage of red blood cell volume is low !");
+
+        } else if (Double.valueOf(str[17]) > 46.0) {
+            checkHematocrit.setText("Hematocrit- this test counting the percentage of red blood cell volume - The percentage of red blood cell volume is high !");
+
+        } else{
+            checkHematocrit.setText("Hematocrit- this test counting the percentage of red blood cell volume - The percentage of red blood cell volume is at the normal range."); }
+    }
+
+    private void checkValuesMCV( String[] str){
+
+        if (Double.valueOf(str[18]) < 79.0) {
+            checkMCV.setText("MCV- The test counts the volume of red blood cells - your volume of red blood cells ​​are below the normal range!");
+
+        } else if (Double.valueOf(str[18]) > 97.0) {
+            checkMCV.setText("MCV- The test counts the volume of red blood cells- your volume of red blood cells are above the normal range! ");
+
+        } else{
+            checkMCV.setText("MCV- The test counts the volume of red blood cells - your volume of red blood cells ​​are at the normal range"); }
+    }
+
+    private void checkValuesMCH( String[] str){
+
+        if (Double.valueOf(str[19]) < 27.0) {
+            checkMCH.setText("MCH- The test checking the amount and concentration of hemoglobin in the blood cells - the concentration of hemoglobin in your blood cells are low!");
+
+        } else if (Double.valueOf(str[19]) > 34.0) {
+            checkMCH.setText("MCH- The test checking the amount and concentration of hemoglobin in the blood cells - the concentration of hemoglobin in your blood cells are high! ");
+
+        } else{
+            checkMCH.setText("MCH- The test checking the amount and concentration of hemoglobin in the blood cells - the concentration of hemoglobin in your blood cells are at the normal range."); }
+    }
+
+    private void checkValuesMCHC( String[] str){
+
+        if (Double.valueOf(str[20]) < 31.0) {
+            checkMCHC.setText("MCHC- The test reflects the amount of hemoglobin relative to the cell size for each red blood cell - The amount of hemoglobin relative to the cell size is lower than the normal range!");
+
+        } else if (Double.valueOf(str[20]) > 36.0) {
+            checkMCHC.setText("MCHC- The test reflects the amount of hemoglobin relative to the cell size for each red blood cell - The amount of hemoglobin relative to the cell size is higher than the normal range! ");
+
+        } else{
+            checkMCHC.setText("MCHC- The test reflects the amount of hemoglobin relative to the cell size for each red blood cell - The amount of hemoglobin relative to the cell size is at the normal range"); }
+    }
+
+    private void checkValuesRDW( String[] str){
+
+        if (Double.valueOf(str[21]) < 11.5) {
+            checkRDW.setText("RDW- Testing the variability of red blood cell volumes - the variability of red blood cell volumes is below the normal range!");
+
+        } else if (Double.valueOf(str[21]) > 15.0) {
+            checkRDW.setText("RDW- Testing the variability of red blood cell volumes - the variability of red blood cell volumes is above the normal range! ");
+
+        } else{
+            checkRDW.setText("RDW- Testing the variability of red blood cell volumes - the variability of red blood cell volumes is at the normal range"); }
+    }
+
+    private void checkValuesPlatelets( String[] str){
+
+        if (Double.valueOf(str[22]) < 150.0) {
+            checkPlatelets.setText("Platelets- Count the amount of platelets in the blood - The amount of platelets in your blood is low!");
+
+        } else if (Double.valueOf(str[22]) > 450.0) {
+            checkPlatelets.setText("Platelets- Count the amount of platelets in the blood - The amount of platelets in your blood is high!");
+
+        } else{
+            checkPlatelets.setText("Platelets- Count the amount of platelets in the blood - The amount of platelets in your blood are at the normal range"); }
+    }
+
+    private void checkValuesMPV( String[] str) {
+
+        if (Double.valueOf(str[23]) < 8.5) {
+            checkMPV.setText("MPV- The test measures an average platelet volume - The average blood platelet volume in your blood is low!");
+
+        } else if (Double.valueOf(str[23]) > 12.9) {
+            checkMPV.setText("MPV- The test measures an average platelet volume - The average blood platelet volume in your blood is high!");
+
+        } else {
+            checkMPV.setText("MPV- The test measures an average platelet volume - The average blood platelet volume in your blood is at the normal range");
+        }
+
+    }
+
 
 
     private void openTestsPage() {
