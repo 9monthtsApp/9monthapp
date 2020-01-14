d9d00146d2ed18914bfcd664299be03ad6c38ce0
Maycohen60 <57867185+Maycohen60@users.noreply.github.com>
2020-01-14 17:59:36 +0200

    adding general recommendations addind blood test recommendations

--- app/src/main/java/com/first/a9monthsproject/recommendation.java
+++ app/src/main/java/com/first/a9monthsproject/recommendation.java
@@ -1,11 +1,11 @@
 package com.first.a9monthsproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class recommendation extends AppCompatActivity {
 
@@ -18,9 +18,9 @@ public class recommendation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommendation);
 
-        urineRec = findViewById(R.id.urine);
-        bloodRec = findViewById(R.id.blood);
-        generalRec= findViewById(R.id.general);
+        urineRec = findViewById(R.id.urineRes);
+        bloodRec = findViewById(R.id.bloodRes);
+        generalRec= findViewById(R.id.generalInfo);
 
         urineRec.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,10 +28,32 @@ public class recommendation extends AppCompatActivity {
                 openUrineRecommendation();
             }
         });
+        bloodRec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBloodRecommendation();
+            }
+        });
+
+        generalRec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGeneralRecommendation();
+            }
+        });
     }
 
     private void openUrineRecommendation() {
         Intent in = new Intent( this,main_recommendation_urine.class);
+        startActivity(in);
+    }
+
+    private void openBloodRecommendation() {
+        Intent in = new Intent( this,main_recommendation_blood.class);
+        startActivity(in);
+    }
+    private void openGeneralRecommendation() {
+        Intent in = new Intent( this,main_recommendation_general.class);
         startActivity(in);
     }
 }
